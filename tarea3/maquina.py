1a1d70c3fcfd25d274b4dae3444ae9a50767d458
jrodri92 <jrodri92@eafit.edu.co>
2018-08-23 20:28:48 -0500

    terminada primera parte de la creacinnn de la maquina

--- tarea3/maquina.py
+++ tarea3/maquina.py
@@ -2,7 +2,7 @@ def menu():
   return True
   
 def constructorMaquina(mantisa):
-  if(mantisa < 0 or mantisa >= 14):
+  if(mantisa <= 0 or mantisa >= 14):
     print("numero incorrecto: este debe ser mayor que 0 y menor que 14")
     return False
   else:
@@ -19,4 +19,17 @@ if __name__ == "__main__":
     mantisa = int(input())
     if(constructorMaquina(mantisa)):
       break
+  Memory = [16]
+  DSMantisa = 0 #Direccion del signo de la mantisa
+  DSExp = 1 #Direccion del signo del exponente
+  Dmantisa = 2 #Direccion donde comienza la mantisa
+  mantisa = mantisa - 1 # Tamaño de la mantisa
+  Dexp = 2 + mantisa #Direccion donde comienza el exp ( este termina en la casilla 0)
+
+  print("Esta maquina esta construida de esta forma:")
+  print("Signo mantisa: 1 bit")
+  print("Signo exponente: 1 bit")
+  print("Mantisa:", mantisa+1," bits (el bit mas significativo es un bit implicito)")
+  print("Exponente: ", 14-(mantisa) ," bits")
+
     
