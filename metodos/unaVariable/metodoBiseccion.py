669e4fa5a407b711701dd2e3b2d9f0ffb101c4c1
gfernan6 <gfernan6@eafit.edu.co>
2018-09-20 14:00:43 -0500

    Add files via upload

---
+++ metodos/unaVariable/metodoBiseccion.py
@@ -0,0 +1,97 @@
+from __future__ import division
+from sympy import *
+from sympy.parsing.sympy_parser import parse_expr
+from tabulate import tabulate
+import numpy as np
+
+f = Function('fx')
+
+def recolectarDatos():
+    global f
+    f = parse_expr(input("Ingrese la función f(x) a ser evaluada: "))
+    print("La función es: " + str(f))
+    xi = input("Ingrese el extremo inferior: ")
+    xs = input("Ingrese el extremo superior: ")
+    tolerancia = input("Ingrese la tolerancia: ")
+    print("La tolerancia es: " + str(tolerancia))
+    while (tolerancia == 0):
+        print("La tolerancia debe ser diferente de 0, ingresela nuevamente")
+        tolerancia = input("Ingrese la tolerancia: ")
+
+    niteraciones = int(input("Ingrese el numero maximo de iteraciones: " ))
+    print("\n")
+
+    while (niteraciones<0):
+        print("El numero de iteraciones debe ser mayor que 0, ingreselo nuevamente")
+        niteraciones = int(input("Ingrese el numero máximo de iteraciones: "))
+
+    metodoBiseccion(float(xi),float(xs),float(tolerancia),niteraciones)
+
+def metodoBiseccion(xi,xs,tolerancia, niteraciones):
+    global f
+    v_array_aitken = []
+    x = Symbol('x')
+    fxi = f.subs(x,xi)
+    fxs = f.subs(x,xs)
+    if fxi == 0:
+        print(str(xi) + " es una raiz")
+
+    elif fxs == 0:
+        print(str(xs) + " es una raiz")
+
+    elif fxi * fxs > 0:
+        print("El intervalo no posee una raiz")
+
+    else:
+        file = open("biseccion.txt", "w")
+        v_array_imprimir = []
+        xm = (xi + xs) / 2
+        fxm = f.subs(x,xm)
+        v_array_aitken.append([xm,fxm])
+        print( str(1) + "|" + str(xi) + "|" + str(fxi) + "|" + str(xs) + "|" + str(fxs) + "|" + str(xm) + "|" + str(fxm) + "\n")
+        cont = 1
+        errorAbs = tolerancia + 1
+        v_array_imprimir.append([cont, xi, fxi , xs, fxs, xm, fxm, errorAbs, 0])
+        while fxm != 0 and errorAbs > tolerancia and cont < niteraciones:
+            if fxi * fxm < 0:
+                xs = xm
+                fxs = f.subs(x,xs)
+            else:
+                xi = xm
+                fxi = f.subs(x,xi)
+
+            xaux = xm
+            xm = (xi + xs) / 2
+            fxm = f.subs(x,xm)
+            v_array_aitken.append([xm,fxm])
+            errorAbs = abs(xm - xaux)
+            errorRel = errorAbs/xm
+            cont += 1
+            print(str(cont) + "|" + str(xi) + "|" + str(fxi) + "|" + str(xs) + "|" + str(fxs) + "|" + str(xm) + "|" + str(fxm) + "|" + str(errorAbs) + "|" + str(errorRel) + "\n")
+            v_array_imprimir.append([cont, xi, fxi , xs, fxs, xm, fxm, errorAbs, errorRel])
+
+        if fxm == 0:
+            print (str(xm) + " es una raiz")
+            file.write(tabulate(v_array_imprimir, headers=['Iteraciones', 'xi', 'f(xi)', 'xs', 'f(xs)', 'xm', 'fxm','EA', 'ER'], tablefmt='grid'))
+        elif errorAbs < tolerancia:
+            print(str(xm) + " se aproxima a una raiz de la función, con una tolerancia de: " + str(tolerancia))
+            file.write(tabulate(v_array_imprimir, headers=['Iteraciones', 'xi', 'f(xi)', 'xs', 'f(xs)', 'xm', 'fxm','EA', 'ER'], tablefmt='grid'))
+        else:
+            print("Excedio el numero de iteraciones posible")
+        for i in range(0,len(v_array_aitken)):
+            v_array_aitken[i] = np.pad(v_array_aitken[i], (0, len(v_array_aitken)-1), 'constant')
+        n = len(v_array_aitken)
+        #i j donde es
+        #i j-1 es fx1
+        #i-1 j-1 es fx0
+        #i 0 es x1
+        #i-j+1 es x0
+        for j in range(2,n+1):
+            for i in range(j-1,n):
+                v_array_aitken[i][j] = (xm - v_array_aitken[i-j+1][0])*v_array_aitken[i][j-1] - (xm - v_array_aitken[i][0])*v_array_aitken[i-1][j-1]/(v_array_aitken[i][0] - v_array_aitken[i-j+1][0])
+        print("Algoritmo de aitken")
+        print(tabulate(v_array_aitken, tablefmt='grid'))
+        file.write("Algoritmo de aitken")
+        file.write(tabulate(v_array_aitken, tablefmt='grid'))
+        file.close()
+recolectarDatos()
