61876acf36e32dadaeabcdf06e9e86ac97161cf1
jrodri92 <jrodri92@eafit.edu.co>
2018-10-16 02:59:51 -0500

    terminado y sin errores cholesky

--- metodos/python/multiplesVariables/cholesky.py
+++ metodos/python/multiplesVariables/cholesky.py
@@ -1,140 +1,91 @@
-import numpy as np
+from numpy import * 
 from sympy import *
-import sys
-import math
+from tabulate import tabulate
 
-A = []
-L = []
-U = []
-B = []
-Y = []
-X = []
-
-def imprimirMatriz(l, U, n):
-    print("Matriz L")
-    for i in range(n):
-        print(str(L[i]).replace("'"," ").replace(","," "))
-    print(" ")
-    print("Matriz U")
-    for i in range(n):
-        print(str(U[i]).replace("'"," ").replace(","," "))
-
-
-
-def resolverMatriz(L,U,B,n):
-    
-    Y =  [0] * n
-    X = [0] * n
-    print(" ")
-    print("Y")
-    print(Y)
-    for p in range(n):
-        for k in range(n):
-            if k == 0 and p == 0:
-                Ymn = B[k] / L[k][p]
-                Y[k] = Ymn
-            elif k == p:
-                Ymn = B[k]
-                for t in range(k):
-                    Ymn = Ymn - Y[t]*L[k][t]                 
-                Ymn = Ymn / L[k][p]
-                Y[k] = Ymn
-    print(" ")
-    print("Y")
-    print(Y)
-
-    for p in range(n-1,-1,-1):
-        for k in range(n-1,-1,-1):
-            if k == 3 and p == 3:
-                Xmn = Y[k] / U[k][p]
-                X[k] = Xmn                
-            elif k == p:
-                Xmn = Y[k]
-                for t in range(1,4):                    
-                    Xmn = Xmn - X[t]*U[k][t]               
-                Xmn = Xmn / U[k][p]
-                X[k] = Xmn
-    print(" ")
-    print("X")
-    print(X)
-
-
-def conversionL(A,L,U,B,n):
-
-    for y in range(n):        
-        for x in range(n):              
-            if y == x:
-                if y == 0:
-                    Lmn = float(math.sqrt(A[x][y]))
-                    L[x][y] = Lmn 
-                    U[x][y] = Lmn
-                else:
-                    Umn = A[x][y]
-                    for t in range(y):
-                        Umn = Umn - L[x][t]*U[t][x]                        
-                    Umn = float(math.sqrt(Umn))
-                    L[x][y] = Umn
-                    U[x][y] = Umn
-
-
-            elif x > y:
-                if y == 0:
-                    Lxy = A[x][y] / Lmn
-                    L[x][y] = Lxy
-                else:
-                    Lxy = A[x][y]
-                    for t in range(y):
-                        Lxy = Lxy - L[x][t]*U[t][y]
-                    
-                    Lxy = Lxy / U[y][y]
-                    L[x][y] = Lxy
-
-
-            elif y > x:
-                if x == 0:
-                    Uxy = A[x][y] / Lmn
-                    U[x][y] = Uxy
-                else:
-                    Uxy = A[x][y]
-                    for t in range(y):
-                        Uxy = Uxy - L[x][t]*U[t][y]
-                    Uxy = Uxy / L[x][x]
-                    U[x][y] = Uxy         
-    
-    imprimirMatriz(L,U,n)
-    resolverMatriz(L,U,B,n)
-
-    
-
-def inputYverificacion():
-
-    n = int(input("Ingrese el numero de ecuaciones: "))  #Conociendo el numero de ecuaciones, conoceremos las dimensiones de la matriz
-    A = []  
-
+def recolectarDatos():
+    n = int(input("Ingrese el numero de ecuaciones: "))#Conociendo el numero de ecuaciones, conoceremos las dimensiones de la matriz
+    A = []
+    B = []
+    L = []
+    U = []
     for i in range(n):
         A.append([0] * (n))    #Se le agregan n+1 columnas a la matriz por los terminos independientes
+        L.append([0]*n)
+        U.append([0]*n)
         fila = str(input("Ingrese los valores de la fila " + str(i+1) + " separados por espacio: "))  #El usuario ingresa los coeficientes de la matriz
         valores = fila.split(" ")
         for j in range(n):       #Es n+1 por la columna de terminos independientes
             A [i][j] = float(valores[j])  #Asignamos a la posición correspondiente de la matriz los coeficientes
-
-    B = [0] * (n)
     columnas = str(input("Ingrese los valores de B separados por espacio: "))
     val = columnas.split(" ")
-    print(B)
     for j in range(n):
-        B[j] = float(val[j])
+        B.append(float(val[j]))
+    print("\nMATRIZ ORIGINAL A")
+    print(tabulate(A, tablefmt='fancy_grid',floatfmt=".14f"))
+    print("\nMATRIZ ORIGINAL B")
     print(B)
 
-    for i in range(m):
-        L.append([])
-        U.append([])
-        for j in range(n):
-            L[i].append(0)
-            U[i].append(0)
+    factorizacionLU(A, L, U, B, n)
 
-    conversionL(A, L, U, B, n)
+def factorizacionLU(A,L,U,B,n):
+    for k in range (n):
+        for i in range(k,n):
+            if(i == k):
+                sumatoria = 0
+                for p in range(k):
+                    sumatoria += L[k][p]*U[p][k]
+                L[k][k] = (A[k][k] - sumatoria)**(1/2)
+            else:
+                sumatoria = 0
+                for p in range(k):
+                    sumatoria += L[i][p]*U[p][k]
+                L[i][k]= (A[i][k]-sumatoria)/L[k][k]
+        for j in range(k,n):
+            if( j == k):
+                U[k][k] = L[k][k]
+            else:
+                sumatoria = 0
+                for p in range(k):
+                    sumatoria += L[k][p]*U[p][j]
+                U[k][j]= (A[k][j]-sumatoria)/L[k][k]
+        print("\nETAPA"+str(k+1))
+        print("\nMATRIZ L\n")
+        print(tabulate(L, tablefmt='fancy_grid',floatfmt=".14f"))
+        print("\nMATRIZ U\n")
+        print(tabulate(U, tablefmt='fancy_grid',floatfmt=".14f"))
+    sustitucionProgresiva(L,B,U,n)     
 
-                
-inputYverificacion()
+def sustitucionProgresiva(L,B,U, n):
+    print("\nSUSTITUCION PROGRESIVA")   
+    print("CALCULANDO Lz=B")
+    Z = []
+    for i in range(n):
+        Z.append(0)
+    Z[0]=(B[0]/L[0][0])
+    print("Z1""= "+str(Z[0]))
+    for i in range(1,n):
+        sum = 0
+        for j in range(i+1):
+            if i==j:
+               Z[j] = (B[j]-sum)/L[i][j]
+               print("Z"+str(i+1)+"= "+str(Z[j]))
+            else:
+                sum += L[i][j]*Z[j]
+    sustitucionRegresiva(U,Z,n)
+        
+def sustitucionRegresiva(U,Z, n):
+    print("\nSUSTITUCION REGRESIVA")   
+    print("CALCULANDO Ux=Z")
+    X = []
+    for i in range(n):
+        X.append(0)
+    for i in range(n-1,0-1,-1):
+        sum = 0
+        for j in range(i,n):
+           sum += U[i][j]*X[j]
+        X[i] = ((Z[i]-sum)/U[i][i])
+    
+    for i in range(n):
+        print("X"+str(i+1)+": "+str(X[i]))
 
+recolectarDatos()
